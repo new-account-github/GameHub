01190fdacc21aebdd906515ae7ba451109f0aa23
HoangMDung <wikodung123@gmail.com>
2023-10-16 16:13:39 +0700

    gg1
    load dau tien

--- src/main/java/com/gamehub/entity/Account.java
+++ src/main/java/com/gamehub/entity/Account.java
@@ -2,14 +2,12 @@ package com.gamehub.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
--- src/main/java/com/gamehub/repository/GameRepository.java
+++ src/main/java/com/gamehub/repository/GameRepository.java
@@ -4,6 +4,5 @@ import com.gamehub.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface GameRepository extends JpaRepository<Game,Long> {
 }
