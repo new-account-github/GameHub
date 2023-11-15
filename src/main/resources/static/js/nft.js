1980e56eafc934bd4b796db6ba5e1893a44f68b1
KimVanCuong <vancuo1702asd@gmail.com>
2023-11-15 15:38:08 +0700

    createNFT

--- src/main/resources/static/js/nft.js
+++ src/main/resources/static/js/nft.js
@@ -1,54 +1,54 @@
-   async function submitForm(event) {
-        event.preventDefault();
+async function submitForm(event) {
+  event.preventDefault();
 
+  const walletAddress = document.getElementById("wallet-address").value;
+  const nftName = document.getElementById("name-nft").value;
+  const nftSymbol = document.getElementById("symbol-nft").value;
+  const nftDescription = document.getElementById("description").value;
+  const maxSupply = document.getElementById("max-supply").value;
+  const royalty = document.getElementById("royalty").value;
+  const fileNft = document.getElementById("file-nft").files[0];
+  const fileName = fileNft.name;
 
-        const walletAddress = document.getElementById('wallet-address').value;
-        const nftName = document.getElementById('name-nft').value;
-        const nftSymbol = document.getElementById('symbol-nft').value;
-        const nftDescription = document.getElementById('description').value;
-        const maxSupply = document.getElementById('max-supply').value;
-        const royalty = document.getElementById('royalty').value;
-        const fileNft = document.getElementById('file-nft').files[0];
-        const attributes = [
-          { "trait_type": "edification", "value": "100" },
-        ];
+  console.log(walletAddress);
+  console.log(nftName);
+  console.log(nftSymbol);
+  console.log(nftDescription);
+  console.log(maxSupply);
+  console.log(royalty);
+  console.log(fileNft);
 
-        const url = 'https://api.shyft.to/sol/v1/nft/create_detach';
-        const formData = new FormData();
-        formData.append('network','devnet');
-        formData.append('wallet', walletAddress);
-        formData.append('receiver', walletAddress);
-        formData.append('name', nftName);
-        formData.append('symbol', nftSymbol);
-        formData.append('description', nftDescription);
-        formData.append('max_supply', maxSupply);
-        formData.append('royalty', royalty);
-        formData.append('file', fileNft);
-        formData.append('external_url','https://shyft.to')
-        formData.append('data','')
-        formData.append('service_charge','')
-        attributes.forEach((attribute, index) => {
-          const traitTypeKey = `attributes[${index}][trait_type]`;
-          const valueKey = `attributes[${index}][value]`;
+  var myHeaders = new Headers();
+  myHeaders.append("x-api-key", "0Bt3GbCL2ul_fOVH");
 
-          formData.append(traitTypeKey, attribute.trait_type);
-          formData.append(valueKey, attribute.value);
-        });
+  var formdata = new FormData();
+  formdata.append("network", "devnet");
+  formdata.append(
+    "creator_wallet",walletAddress
+    
+  );
+  formdata.append("name", nftName);
+  formdata.append("symbol", nftSymbol);
+  formdata.append("description", nftDescription);
+  formdata.append(
+    "attributes",
+    '[{"trait_type":"dev power","value":"over 900"}]'
+  );
+  formdata.append("external_url", "https://shyft.to");
+  formdata.append("max_supply", maxSupply);
+  formdata.append("royalty", royalty);
+  formdata.append("image", fileNft, fileName);
+  formdata.append("fee_payer", walletAddress);
 
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formdata,
+    redirect: "follow",
+  };
 
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                body: formData,
-            });
-
-            if (response.ok) {
-                const result = await response.json();
-                console.log('Success:', result);
-            } else {
-                console.error('Error:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    }
+  fetch("https://api.shyft.to/sol/v2/nft/create", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
