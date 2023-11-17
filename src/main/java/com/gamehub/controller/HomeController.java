249da6a9286f91d1780ba7a132cc57bf14055390
KimVanCuong <vancuo1702asd@gmail.com>
2023-11-17 09:58:06 +0700

    all isAuthenticated

--- src/main/java/com/gamehub/controller/HomeController.java
+++ src/main/java/com/gamehub/controller/HomeController.java
@@ -13,16 +13,6 @@ public class HomeController {
 
 	@GetMapping("/home")
 	public String index(Model model) {
-		String username = request.getRemoteUser();
-		if (username != null && (request.isUserInRole("ADMIN")|| request.isUserInRole("STAFF"))){
-			model.addAttribute("isAuthenticated",true);
-			model.addAttribute("isAdmin",true);
-		} else if(username != null){
-			model.addAttribute("isAuthenticated",true);
-		} else {
-			model.addAttribute("isAuthenticated",false);
-			model.addAttribute("isAdmin",false);
-		}
 		return "user/index";
 	}
 }
