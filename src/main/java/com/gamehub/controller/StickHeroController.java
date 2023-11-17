9a4b7e9925567fbe8bd984cea878df5e280c09e2
HoangMDung <wikodung123@gmail.com>
2023-11-17 10:04:41 +0700

    cde

--- src/main/java/com/gamehub/controller/StickHeroController.java
+++ src/main/java/com/gamehub/controller/StickHeroController.java
@@ -14,16 +14,6 @@ public class StickHeroController {
 	private HttpServletRequest request;
 	@GetMapping("/stickhero")
 	public String flappyBird(Model model) {
-		String username = request.getRemoteUser();
-		if (username != null && (request.isUserInRole("ADMIN")|| request.isUserInRole("STAFF"))){
-			model.addAttribute("isAuthenticated",true);
-			model.addAttribute("isAdmin",true);
-		} else if(username != null){
-			model.addAttribute("isAuthenticated",true);
-		} else {
-			model.addAttribute("isAuthenticated",false);
-			model.addAttribute("isAdmin",false);
-		}
 		return "user/stickhero";
 	}
 }
